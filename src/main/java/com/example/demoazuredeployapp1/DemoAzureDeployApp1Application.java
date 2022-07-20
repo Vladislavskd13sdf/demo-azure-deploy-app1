75603d2871e493256e2d4c19b920365de04b0451
Vladislavskd13sdf <koshulkov@gmail.com>
2022-07-20 16:44:29 +0300

    Initial commit

---
+++ src/main/java/com/example/demoazuredeployapp1/DemoAzureDeployApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.demoazuredeployapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAzureDeployApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAzureDeployApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoazuredeployapp1/DemoAzureDeployApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoazuredeployapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAzureDeployApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
